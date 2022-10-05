2e64d2636a503246c625421c26b57fb9272fb648
Riccardo Gulin <riccardo@nucleode.com>
2022-10-05 10:31:54 +0200

    Adds basic Express config

---
+++ src/server.js
@@ -0,0 +1,9 @@
+//const express = require("express") <-- OLD SYNTAX (we don't want to use old stuff)
+import express from "express" // NEW SYNTAX (you can use this only if type:"module" is added on package.json)
+
+const server = express()
+const port = 3002
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
